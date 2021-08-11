cdb2f9979b3b16a370b040369d16e80d16cd3efa
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-11 17:21:30 +0430

    Update persian.py

--- persian.py
+++ persian.py
@@ -56,7 +56,11 @@ async def loader(bot, message):
             Y = m.split(None, 8)[8]
     else:
         await message.reply("فک کنم سال ساخت فیلم رو وارد نکردی")
-    N = message.text.replace(" ", ".")
+    if "And" or "Of" or "The" in m:
+        if m.startswith("And") or m.startswith("Of") or m.startswith("The"):
+            N = message.text.replace(" ", ".").message.text.replace("And", "and").message.text.replace("Of", "of").message.text.replace("The", "the") 
+    else:
+        N = message.text.replace(" ", ".")
     link = f"https://dl.worldsubtitle.site/wrpink/Movies/{Y}/{N}_WorldSubtitle.zip"
     await message.reply(link)
     
