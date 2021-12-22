43778030e551bfbf47a9be6416d8603f2e704cbd
Soebb <87156166+Soebb@users.noreply.github.com>
2021-12-22 12:56:10 +0330

    Update persian.py

--- persian.py
+++ persian.py
@@ -6,6 +6,8 @@ API_HASH = os.environ['API_HASH'] # Api hash
 API_ID = os.environ['API_ID'] # Api id/App id
 BOT_TOKEN = os.environ['BOT_TOKEN'] # Bot token
 
+dirs = 'dl/'
+
 
 # Running bot
 xbot = Client(
@@ -25,32 +27,36 @@ START_MSG = """
 A Savage Nature 2020
 
 Fast And Furious 9 2021
-
 """
+
 
 @xbot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
     await message.reply(START_MSG)
 
+
 @xbot.on_message(filters.private & filters.text & ~filters.regex('/start'))
 async def subdl(bot, message):
+
+    if not os.path.isdir(dirs):
+        os.makedirs(dirs)
+
+    year = message.text.split()[-1]
+    if not year.__contains__("20") and not year.__contains__("19"):
+        await message.reply("سال ساخت فیلم رو وارد نکردی")
+
     splited = message.text.split()[:-1]
     title = ''
     for i in range(len(splited)):
         s = splited[-1+i]
-        title += s[:1].upper() + s[1:len(s)].lower() + '.' if i != len(splited) else ''
-    year = message.text.split()[-1]
-    if not year.__contains__("20") and not year.__contains__("19"):
-        await message.reply("سال ساخت فیلم رو وارد نکردی")
-    if not title.startswith("And") or title.startswith("Of") or title.startswith("The") or title.startswith("With"):
+        title += s[:1].upper() + s[1:len(s)].lower() + '.'
+
+    if not title.startswith(("And", "Of", "The", "With")):
         title = title.replace("And", "and").replace("Of", "of").replace("With", "with").replace("The", "the") 
-    
-    link = f"https://dl.worldsubtitle.site/wrpink/Movies/{year}/{title}_WorldSubtitle.zip"
-    dirs = f'dl/'
-    if not os.path.isdir(dirs):
-        os.makedirs(dirs)
-    dldir = f'{dirs}{N}.zip'
+
+    link = f"https://dl.worldsubtitle.site/wrpink/Movies/{year}/{title}{year}_WorldSubtitle.zip"
     r = requests.get(link, allow_redirects=True, headers={'User-Agent': 'Mozilla/5.0'})
+    dldir = f'{dirs}{title}zip'
     if r.status_code == 200:
         f = open(dldir, 'wb')
         f.write(r.content)
